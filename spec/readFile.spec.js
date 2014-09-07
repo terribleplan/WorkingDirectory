e774b67bdc9f145a3cc7eb8d8142c82084ab81ec
Kegan Myers <kegan@keganmyers.com>
2014-09-07 17:03:26 -0500

    Fix naming and filenames

--- spec/readFile.spec.js
+++ spec/readFile.spec.js
@@ -6,7 +6,7 @@ describe("readFile functionality", function() {
     var _this = this;
     require('./harness.js')(_this);
     it("reads a file", function(done) {
-        var fname = "testDir";
+        var fname = "testFile.txt";
         var resolved = path.resolve(_this.tempdir, fname);
         crypto.pseudoRandomBytes(128 * 1024, function(err, data) {
             if (err) throw err;
--- spec/writeFile.spec.js
+++ spec/writeFile.spec.js
@@ -6,7 +6,7 @@ describe("writeFile functionality", function() {
     var _this = this;
     require('./harness.js')(_this);
     it("writes data to a file that doesn't exist", function(done) {
-        var fname = "testDir";
+        var fname = "testFile.txt";
         var resolved = path.resolve(_this.tempdir, fname);
         crypto.pseudoRandomBytes(128 * 1024, function(err, data) {
             if (err) throw err;
@@ -16,6 +16,28 @@ describe("writeFile functionality", function() {
                     if (err) throw err;
                     expect(rdata.toString()).toBe(data.toString());
                     done();
+                });
+            });
+        });
+    });
+    it("overwrites a file that does exist", function(done) {
+        var fname = "testFile.txt";
+        var resolved = path.resolve(_this.tempdir, fname);
+        crypto.pseudoRandomBytes(128 * 1024, function(err, initialData) {
+            if (err) throw err;
+            fs.writeFile(resolved, initialData, function(err) {
+                if (err) throw err;
+                crypto.pseudoRandomBytes(128 * 1024, function(err, data) {
+                    if (err) throw err;
+                    _this.wd.writeFile(fname, data, function(err) {
+                        expect(err).toBe(null);
+                        fs.readFile(resolved, function(err, rdata) {
+                            if (err) throw err;
+                            expect(rdata.toString()).not.toBe(initialData.toString());
+                            expect(rdata.toString()).toBe(data.toString());
+                            done();
+                        });
+                    });
                 });
             });
         });
